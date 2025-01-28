9da0ef1c70fd2d36ab36ba4f46e5268b962e9e6e
Guru043 <139951342+Guru043@users.noreply.github.com>
2025-01-28 12:10:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-const signUpButton=document.getElementById('signUpButton');
-const signInButton=document.getElementById('signInButton');
-const signInForm=document.getElementById('signIn');
-const signUpForm=document.getElementById('signup');
-
-signUpButton.addEventListener('click',function(){
-    signInForm.style.display="none";
-    signUpForm.style.display="block";
-})
-signInButton.addEventListener('click', function(){
-    signInForm.style.display="block";
-    signUpForm.style.display="none";
-})
+function toggleForms(formType) {
+    document.getElementById("login-form").style.display = formType === "login" ? "block" : "none";
+    document.getElementById("register-form").style.display = formType === "register" ? "block" : "none";
+}
